9a20363099e25b04acdd84c2df1ec1a1b4ed3b92
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-17 16:40:54 -0400

    Add form content and images.

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -10,7 +10,57 @@ export default function Form() {
             onSubmit={handleSubmit}
         >
             
+            <img />
 
+            <fieldset>
+
+                <h1>
+                    Stay Updated!
+                </h1>
+
+                <p>
+                    Join 60,000&#43; product managers receiving monthly updates on:
+                </p>
+
+                <ul>
+
+                    <li>
+                        Product discovery and building what matters
+                    </li>
+
+                    <li>
+                        Measuring to ensure updates are a success
+                    </li>
+
+                    <li>
+                        And much more!
+                    </li>
+
+                </ul>
+
+                <label htmlFor="email">
+
+                    <span>
+                        Email address
+                    </span>
+
+                    <input 
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="email@company.com"
+                        required
+                    />
+
+                </label>
+
+                <button>
+
+                    Subscribe to monthly newsletter
+                    
+                </button>
+
+            </fieldset>
 
         </form>
     )
